61bf58f1b1171a65b923ca87e3eedaeb54f1a27b
Guilherme Gomes <guisalvadeo198@gmail.com>
2026-03-30 20:48:24 -0300

    Cadastro finalizada

--- Projetomobile/app/src/main/java/com/example/erectus/CadastroActivity.java
+++ Projetomobile/app/src/main/java/com/example/erectus/CadastroActivity.java
@@ -4,9 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class CadastroActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Isso aqui liga o código à sua tela visual de cadastro
         setContentView(R.layout.activity_cadastro);
+
+        // A lógica de pegar os dados digitados e salvar o usuário vai entrar aqui depois!
     }
 }
